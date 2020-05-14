951664a32b7c53c2140a3583584256ea85f3887b
Eliud Kagema <eliud.k.maina@gmail.com>
2020-05-14 19:17:22 +0300

    create views and urls

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Order
+
+@admin.register(Order)
+class  OrderAdmin(admin.ModelAdmin):
+    list_display = ['product_category', 'payment_method', 'shipping_cost', 'unit_price']
